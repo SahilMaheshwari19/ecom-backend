bd2988447eff7b50c438b7dda93a4ffe7850fced
SahilMaheshwari19 <sahil19singhi@gmail.com>
2025-07-25 02:39:46 +0530

    fix id generation of user

--- src/main/java/com/wryon/ecomproj1/model/Users.java
+++ src/main/java/com/wryon/ecomproj1/model/Users.java
@@ -1,6 +1,8 @@
 package com.wryon.ecomproj1.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +15,7 @@ import lombok.ToString;
 public class Users {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String password;
