215cefaea82c2f90c184179a21c606cba7355eb1
SahilMaheshwari19 <sahil19singhi@gmail.com>
2025-07-25 02:41:05 +0530

    add http-cookie method of sending jwt token

--- src/main/java/com/wryon/ecomproj1/config/JwtFilter.java
+++ src/main/java/com/wryon/ecomproj1/config/JwtFilter.java
@@ -4,6 +4,7 @@ import com.wryon.ecomproj1.service.JWTService;
 import com.wryon.ecomproj1.service.MyUserDetailService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,20 +28,33 @@ public class JwtFilter extends OncePerRequestFilter {
     private ApplicationContext applicationContext;
 
     @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getRequestURI();
+        return path.equals("/login") || path.equals("/register");
+    }
+
+
+    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         String authHeader = request.getHeader("Authorization");
         String jwtToken = null;
         String username = null;
 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            jwtToken = authHeader.substring(7);
-            username = jwtService.extractUserName(jwtToken);
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("jwtToken")) {
+                    jwtToken = cookie.getValue();
+                    username = jwtService.extractUserName(jwtToken);
+                }
+            }
         }
 
         if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
             UserDetails userDetails = applicationContext.getBean(MyUserDetailService.class).loadUserByUsername(username);
+            System.out.println(userDetails.getUsername());
             if(jwtService.validateToken(jwtToken,userDetails)){
                 UsernamePasswordAuthenticationToken authToken =
                         new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
@@ -48,6 +62,10 @@ public class JwtFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
+        System.out.println("Authorization Header: " + authHeader);
+        System.out.println("JWT Token: " + jwtToken);
+        System.out.println("Username: " + username);
+
         filterChain.doFilter(request,response);
     }
 }
