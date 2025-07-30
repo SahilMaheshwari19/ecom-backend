b6f1bd758b360adc5b971ac37c824bb0fb02b9f3
SahilMaheshwari19 <sahil19singhi@gmail.com>
2025-07-30 16:00:21 +0530

    add loggers in UserController

--- src/main/java/com/wryon/ecomproj1/controller/UserController.java
+++ src/main/java/com/wryon/ecomproj1/controller/UserController.java
@@ -6,12 +6,12 @@ import com.wryon.ecomproj1.service.JWTService;
 import com.wryon.ecomproj1.service.MyUserDetailService;
 import com.wryon.ecomproj1.service.UserService;
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +28,8 @@ public class UserController {
     @Autowired
     private MyUserDetailService myUserDetailService;
 
+    Logger LOG = LoggerFactory.getLogger(LoggerFactory.class);
+
     @Autowired
     private JWTService jwtService;
     @Autowired
@@ -35,7 +37,7 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Users users, HttpServletResponse response) {
-        System.out.println("InSide Login Controller Method = " + users.toString());
+        LOG.info("InSide Login Controller Method = {}", users.toString());
         String jwtToken = userService.verify(users);
         if (!jwtToken.equals("fail")) {
             Cookie cookie = new Cookie("jwtToken", jwtToken);
@@ -53,7 +55,7 @@ public class UserController {
             responseBody.put("message", "Login Success");
             responseBody.put("username", user.getUsername());
             responseBody.put("role", user.getRole());
-            System.out.println("Login Controller Method Map response" + responseBody.toString());
+            LOG.info("Login Controller Method Map response{}", responseBody.toString());
             return ResponseEntity.ok(responseBody);
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login Failed -> Invalid Creds");
@@ -62,8 +64,7 @@ public class UserController {
 
     @PostMapping("/logsout")
     public ResponseEntity<?> logout(HttpServletResponse response) {
-        System.out.println("InSide logout Controller Method = ");
-
+        LOG.info("InSide logout Controller Method = ");
         Cookie cookie = new Cookie("jwtToken", null);
         cookie.setHttpOnly(true);
         cookie.setMaxAge(0);
@@ -74,12 +75,12 @@ public class UserController {
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody Users users) {
-        System.out.println("InSide Register Controller Method = " + users.toString());
+        LOG.info("InSide Register Controller Method = {}", users.toString());
         if (myUserDetailService.checkIfUserAlreadyExist(users.getUsername())) {
-            System.out.println("InSide Register Controller Method 1st if block = " + users.toString());
+            LOG.info("InSide Register Controller Method 1st if block = {}", users.toString());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username Already Exist");
         } else {
-            System.out.println("InSide Register Controller Method else block = " + users.toString());
+            LOG.info("InSide Register Controller Method else block = {}", users.toString());
             users.setRole("ROLE_USER");
             userService.registerUsers(users);
             return ResponseEntity.status(HttpStatus.CREATED).body("Registered Successfully");
