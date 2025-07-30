9c76fceb82e75b0d878ac2d02c1d84e0c3fbb597
SahilMaheshwari19 <sahil19singhi@gmail.com>
2025-07-30 15:54:24 +0530

    add admin controller to segregate endpoints based on role

---
+++ src/main/java/com/wryon/ecomproj1/controller/ProductControllerAdmin.java
@@ -0,0 +1,57 @@
+package com.wryon.ecomproj1.controller;
+
+import com.wryon.ecomproj1.model.Product;
+import com.wryon.ecomproj1.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/admin")
+public class ProductControllerAdmin {
+
+    Logger LOG = LoggerFactory.getLogger(LoggerFactory.class);
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile){
+        LOG.info("InSide addProduct Controller Method");
+        try {
+            Product newProduct = productService.addProduct(product, imageFile);
+            return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> UpdateProductById(@PathVariable int id, @RequestPart Product product, @RequestPart(required = false) MultipartFile imageFile ) throws IOException {
+        LOG.info("InSide UpdateProductById Controller Method");
+        Product updatedProduct = productService.UpdateProductById(id, product, imageFile);
+        if(updatedProduct != null){
+            return new ResponseEntity<>("Product Updated Successfully", HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("Product Not Updated Successfully", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProductById(@PathVariable int id){
+        LOG.info("InSide deleteProductById Controller Method");
+        try {
+            productService.deleteProductById(id);
+            return new ResponseEntity<>("Product Deleted Successfully", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>( e.getMessage() + " -- Product Not Deleted Successfully", HttpStatus.NOT_FOUND);
+        }
+    }
+}
